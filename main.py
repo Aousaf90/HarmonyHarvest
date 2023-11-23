f77cd1d0b90a109842f3867804b95a653ce48dfe
Aousaf <aousaf@aousafs-air.home>
2023-11-23 23:48:57 +0500

    feature: further fix the spotify get playlist tracks

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from tkinter import *
+import json
 from tkinter import ttk
 from pathlib import Path
+from bs4 import BeautifulSoup
 import requests
-import re
-import pandas
+import xml.etree.ElementTree as ET
 def get_songs():
     url  = "https://api.spotify.com."
     try:
@@ -18,29 +19,43 @@ def get_songs():
                     "client_id": "fd7fca6bd4824b0091280fb695cd4441",
                     "client_secret": "f94dc958f9174aaaade597dfa444775a" 
                 }
-                post_request = requests.post(url= post_url, headers=post_request_header,data= post_request_data )
+                post_request = requests.post(url = post_url, headers = post_request_header, data = post_request_data )
                 if post_request.status_code == 200:
                     print("API CONNECTED SUCCESSFULLY")
                     DATA = post_request.json()
                     access_token = DATA['access_token']
-                    get_request  = {
-                        "Authorization": access_token
+
+                    # Prepare headers for GET request
+                    headers = {
+                        'Authorization': 'Bearer ' + access_token,
+                        'Accept': 'application/json'
                     }
-                    playlist_url = "https://open.spotify.com/playlist/"
+
+                    # Construct playlist URL
+                    playlist_url = "https://api.spotify.com/v1/playlists/"
                     playlist_id = "3L0NJpTEIAfubSp7uUxcLq?si=5b9b7c124a4d4812"
-                    # TODO: use re instead of simple striping
-                    # playlist_id =  playlist_url.split("/")[-1]
-                    print("Playlist URl: ", playlist_id)
-                    get_response = requests.get(url=playlist_url+playlist_id, headers=get_request)
+                    endpoint = playlist_url + playlist_id + "/tracks"
+
+                    # Send GET request to retrieve playlist data
+                    get_response = requests.get(url=endpoint, headers=headers)
+
+                    # Check for successful response
                     if get_response.status_code == 200:
-                        print("SPOTIFY DATA GET SUCCESSFULLY")
+                        spotify_data = get_response.json()
+                        print(f"Spotify Data = {spotify_data.keys()}")
+                        playlist_name =  spotify_data['name']
+                        tracks_detail = spotify_data['tracks']
+                        tracks = tracks_detail['items']
+                        print(f"Tracks = {tracks}")
+
+                        print(f"Playlist Name = {playlist_name}")
                     else:
-                        print("THERE WAS AN ERROR")
-                        print("ERROR CODE: ", get_response.status_code)
+                        print(f"Error: {get_response.status_code}")
+                else:
+                    print(f"Error: {post_request.status_code}")
     except requests.exceptions.RequestException as e:
-        print("NO request Get")
-        print(f"API request ERROR = {e}")
-get_songs() 
+        RuntimeError(f"API request ERROR = {e}")
+get_songs()
 # Tk = Tk()
 # playlist_link = StringVar()
 # canvas = Canvas(Tk, width= 100, height= 100)
